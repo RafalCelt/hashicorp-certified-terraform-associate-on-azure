ddfa97107993bcd887c7653feb16125fb214afa9
RafalCelt <dsrepair71@gmail.com>
2025-01-07 10:14:57 -0800

    Updated RG name

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
 
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "eastus"
-  name = "my-demo-rg1"  
+  location = "westus2"
+  name = "terraform-udemy"  
 }
