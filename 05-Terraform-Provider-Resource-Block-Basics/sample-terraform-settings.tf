a8e0c3ca3ae9d483ea4fd9691c0952c199f39286
RafalCelt <dsrepair71@gmail.com>
2025-01-14 13:14:43 -0800

    Minor changes

--- 05-Terraform-Provider-Resource-Block-Basics/sample-terraform-settings.tf
+++ 05-Terraform-Provider-Resource-Block-Basics/sample-terraform-settings.tf
@@ -9,13 +9,13 @@ terraform {
     }
   }
   # Terraform State Storage to Azure Storage Container
-  backend "azurerm" { 
+  /*backend "azurerm" { 
     resource_group_name   = "terraform-storage-rg"
     storage_account_name  = "terraformstate201"
     container_name        = "tfstatefiles"
     key                   = "terraform.tfstate"
-  }  
-  experiments = [ example ]# Experimental (Not required)
+  } */ 
+  # experiments = [ example ]# Experimental (Not required)
   provider_meta "my-provider" { # Super Advanced (Not required)
     hello = "world"
   }
